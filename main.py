6a483726fadfe77ea9040527fe838411c4d3916d
hinata <ryugamine020401@gmail.com>
2025-02-19 01:54:09 +0000

    refactor(bot): 重構事件處理為裝飾器註冊
    將事件處理從 class 內直接定義的方式改為使用裝飾器註冊，
    提高可維護性與模組化程度，方便後續替換為 Cog 架構。
    
    這樣的改動使事件處理更加清晰，並為未來的功能擴展做好準備。

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import os
 
@@ -7,68 +8,81 @@ load_dotenv()
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 WELCOME_CHANNEL_ID = int(os.getenv("WELCOME_CHANNEL_ID"))
 
-class Client(discord.Client):
-    async def on_ready(self):
-        """
-        當 bot 啟動時會做的事
-        """
-        print(f"{self.user} Online now!")
-
-    async def on_message(self ,msg):
-        """
-        當 bot 收到聊天室的訊息
-        """
-        if msg.author == self.user:
-            return
-        
-        if msg.content.startswith('hello'):
-            await msg.channel.send(f"hi {msg.author}")
-
-    async def on_message_delete(self, msg):
-        """
-        刪除訊息的事件
-        """
-        if msg.author.bot:
-            return
-
-        channel = self.get_channel(msg.channel.id)
-        await channel.send((f" 刪除了 {msg.author.mention} 在 {msg.channel.name} 說的 : 『{msg.content}』"))
-
-    async def on_member_join(self, member):
-        """
-        伺服器成員加入的事件
-        """
-        welcome_channel_id = WELCOME_CHANNEL_ID
-        channel = self.get_channel(welcome_channel_id)
-        # print(dir(member))
-        
-        try:
-            message = f"(●´ω｀●)ゞ 恭請 {member.global_name} 駕到。"
-            await member.send(message)
-        except Exception as e:
-            print(f"main.py 49, \n{e}")
-
-        await channel.send(f"Welcome {member.global_name} join {member.guild} at {member.joined_at}.\n{member.avatar}")
-        
-    async def on_member_update(self, before, after):
-        """
-        成員編輯
-        """
-        if before.display_name != after.display_name:
-            message = f"抓到偷改名喔 (｡A｡) {before.display_name}"
-        else:
-            message = f"偷改 (｡A｡)"
-        try:
-            await after.send(message)
-            print(f"成功發送歡迎訊息給 {after.name}")
-        except discord.Forbidden:
-            print(f"{after.name} 禁用了私訊，無法發送歡迎訊息。")
-
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
+bot = commands.Bot(command_prefix="$", intents=intents)
 
-client = Client(intents = intents)
-client.run(BOT_TOKEN)
+@bot.event
+async def on_ready():
+    """
+    當 bot 啟動時會做的事
+    """
+    print(f"{bot.user} Online now!")
+
+@bot.event
+async def on_message(msg):
+    """
+    當 bot 收到聊天室的訊息
+    """
+    if msg.author == bot.user:
+        return
+    if msg.content.startswith('hello'):
+        await msg.channel.send(f"hi {msg.author}")
+
+@bot.event
+async def on_message_delete(msg):
+    """
+    刪除訊息的事件
+    """
+    if msg.author.bot:
+        return
+
+    channel = bot.get_channel(msg.channel.id)
+    await channel.send((f" 刪除了 {msg.author.mention} 在 {msg.channel.name} 說的 : 『{msg.content}』"))
+
+@bot.event
+async def on_member_join(member):
+    """
+    伺服器成員加入的事件
+    """
+    welcome_channel_id = WELCOME_CHANNEL_ID
+    channel = bot.get_channel(welcome_channel_id)
+    
+    try:
+        message = f"(●´ω｀●)ゞ 恭請 {member.global_name} 駕到。"
+        await member.send(message)
+    except Exception as e:
+        print(f"main.py 49, \n{e}")
+
+    await channel.send(f"Welcome {member.global_name} join {member.guild} at {member.joined_at}.\n{member.avatar}")
+
+@bot.event
+async def on_member_update(before, after):
+    """
+    成員編輯伺服器的個人資訊時會做的事情
+    """
+    if before.display_name != after.display_name:
+        message = f"抓到偷改名喔 (｡A｡) {before.display_name}"
+    else:
+        message = f"偷改 (｡A｡)"
+    try:
+        await after.send(message)
+        print(f"成功DM {after.name}")
+    except discord.Forbidden:
+        print(f"{after.name} 禁用了DM")
+
+
+
+@bot.command()
+async def test(ctx, arg):
+    await ctx.send(arg) 
+
+from typing import Literal
+@bot.command()
+async def shop(ctx, buy_sell: Literal['buy', 'sell'], amount: Literal[1, 2], *, item: str):
+    await ctx.send(f'{buy_sell.capitalize()}ing {amount} {item}(s)!')
+
+bot.run(token=BOT_TOKEN)
